cac1dffcc907f7881f7699798a368a8d5559d126
connor <connorbell015@gmail.com>
2025-05-05 17:11:49 -0700

    created fernet key derivation function

--- pwtool/auth.py
+++ pwtool/auth.py
@@ -1,4 +1,6 @@
 from argon2 import PasswordHasher
+from argon2.low_level import hash_secret_raw, Type
+import base64
 
 class MasterKeyManager:
     def __init__(self):
@@ -20,3 +22,18 @@ class MasterKeyManager:
 
     def return_stored_hash(self):
         return self.hashed_master_key
+
+def derive_fernet_key_argon2(password, salt):
+    password_bytes = password.encode()
+
+    key = hash_secret_raw(
+        secret=password_bytes,
+        salt=salt,
+        time_cost=3, #2 seems to be standard. time_cost is number of times algo will be computed.
+        memory_cost=131072, #128 MB
+        parallelism=4,
+        hash_len=32, # length of bytes, fernet requires 32
+        type=Type.ID
+    )
+
+    return base64.b64encode(key)
