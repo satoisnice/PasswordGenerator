457aeacf4d10bb9ccc36fa4e601cf8f3eb1eaeac
connor <connorbell015@gmail.com>
2025-05-05 17:32:38 -0700

    removed master_manager class, saving its state is not important

--- pwtool/auth.py
+++ pwtool/auth.py
@@ -1,27 +1,19 @@
 from argon2 import PasswordHasher
 from argon2.low_level import hash_secret_raw, Type
 import base64
+from pathlib import Path
 
-class MasterKeyManager:
-    def __init__(self):
-        self.ph = PasswordHasher()
-        self.hashed_master_key = None
+def derive_master_key(password):
+    return PasswordHasher.hash(password)
 
-    def derive_master_key(self, password):
-        self.hashed_master_key = self.ph.hash(password)
+def verify_master_key(stored_hash, password):
+    try:
+        PasswordHasher.verify(stored_hash, password)
+        return True
+    except Exception as e:
+        print("something went wrong:", e)
+        return False
 
-        return self.hashed_master_key
-
-    def verify_master_key(self, stored_hash, password):
-        try:
-            self.ph.verify(stored_hash, password)
-            return True
-        except Exception as e:
-            print("something went wrong:", e)
-            return False
-
-    def return_stored_hash(self):
-        return self.hashed_master_key
 
 def derive_fernet_key_argon2(password, salt):
     password_bytes = password.encode()
@@ -33,7 +25,27 @@ def derive_fernet_key_argon2(password, salt):
         memory_cost=131072, #128 MB
         parallelism=4,
         hash_len=32, # length of bytes, fernet requires 32
-        type=Type.ID
+        type=Type.ID # Type.I resistant to side-channel attacks? Type.D resistant to GPU/ASIC brute-force. Type.ID is balance of both.
     )
 
     return base64.b64encode(key)
+
+def initial_setup(password=None):
+    salt_exists = False
+    master_exists = False
+    salt_is_blank = True
+    master_is_blank = True
+
+    if Path("master.hash").exists():
+        master_exists = True
+        try:
+            with open("master.hash", "r") as m_hash_file:
+                if m_hash_file.read() == "":
+                    print("master.hash file exists but no master key exists.")
+
+                else:
+                    master_is_blank = False
+                    print("master key already setup.")
+        except FileNotFoundError as e:
+            
+    
