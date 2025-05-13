41243e5935b0ccf451a163251339f7b467a45262
Thenewgrid <H.K2348@outlook.com>
2025-05-12 23:05:41 -0700

    website

---
+++ site/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/download')
+def download():
+    zip_path = '../pwtool.zip'
+    return send_file(
+        zip_path,
+        as_attachment=True,
+        download_name='PWtool',
+        mimetype='application/zip'
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
